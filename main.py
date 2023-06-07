1acb30996b7636c6cce975090c0915bd9c8ae18e
nickek <nickekthou@gmail.com>
2023-06-06 23:17:13 -0400

    changelog and visuals file added

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import database_management
 import User
+import visuals
 
 # Initializing user database
 users = database_management.get_user_data()
 
 for user in users:
-    print(user.username, user.password, user.balance, user.net_profit)
+    print(f"Username: {user.username:<18} Password: {user.password:<18} Balance: {user.balance:>10.2f} "
+          f"\tNet Profit: {user.net_profit:>10.2f}")
+
+visuals.menu()
