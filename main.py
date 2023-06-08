bd6cdd0ade303b3bc06c9a77d43a54fc32fc06b2
secakusumae <secakusumae@wit.edu>
2023-06-08 13:47:22 -0400

    Added login_system.py and added function to main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import database_management
 import User
+import login_system
 import visuals
 
 # Initializing user database
@@ -9,4 +10,6 @@ for user in users:
     print(f"Username: {user.username:<18} Password: {user.password:<18} Balance: {user.balance:>10.2f} "
           f"\tNet Profit: {user.net_profit:>10.2f}")
 
+print("\n")
+login_system.login(users)
 visuals.menu()
