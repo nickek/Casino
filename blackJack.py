e3f321986fa99d30dbeda462943e7fb3aea6f5d3
tenoriom <marceloft02@gmail.com>
2023-08-03 14:42:56 -0400

    First update: adding my current blackjack game from my local file on my laptop, still need to add betting options and such. 06/22/2023 - small update to add another hit action to initial player turn and connected game to database. Also worked out many bugs for different outcomes for the game. Created new dealer action that shows all dealer cards after user stands or bust.

--- blackJack.py
+++ blackJack.py
@@ -1,11 +1,15 @@
 import random
 import sqlite3
-connector = sqlite3.connect("user_database.db") #added to connect game to our user database
+
+connector = sqlite3.connect("user_database.db")
 cursor = connector.cursor()
+
+
 class Card:
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
+
 
 class Deck:
     def __init__(self):
@@ -13,6 +17,7 @@ class Deck:
         ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
         self.cards = [Card(suit, rank) for suit in suits for rank in ranks]
         random.shuffle(self.cards)
+
 
 class Hand:
     def __init__(self):
@@ -39,13 +44,13 @@ class Hand:
 
         return value
 
+
 class Player:
-    def __init__(self, deck, balance):
-        self.balance = balance
+    def __init__(self, deck):
         self.hand = Hand()
         self.deck = deck
+        self.hit()  # Add two cards to player hand
         self.hit()
-        self.hit()  #add two cards to player hand
 
     def hit(self):
         card = self.deck.cards.pop()
@@ -65,12 +70,14 @@ class Player:
 
     def stand(self):
         pass
+
     def value(self):
         return self.hand.get_value()
 
+
 class Dealer(Player):
     def see_card(self):
-        print("Dealer's cards:")
+        print("Dealer's card:")
         print(f"{self.hand.cards[0].rank} of {self.hand.cards[0].suit}")
 
     def final_see_card(self):
@@ -82,92 +89,65 @@ class Dealer(Player):
     def game(self, user):
         while self.hand.get_value() < 17:
             self.hit()
-        #self.see_card()
         if self.hand.get_value() > 21:
             self.final_see_card()
             print("Dealer busts! You win!")
-            #user wins bet
-            user.balance = money + (bet*2)
-
         elif self.hand.get_value() > user.hand.get_value():
             self.final_see_card()
             print("Dealer wins!")
-            user.balance = money - bet
         elif self.hand.get_value() < user.hand.get_value():
             self.final_see_card()
             print("You beat the dealer! You win!")
-            user.balance = money + (bet * 2)
         else:
             self.final_see_card()
             print("It's a tie!")
-            money = money + bet
+
 
 def main(user):
     random.seed()
-    print("Your balance:", user.balance)
     print('Lets play Blackjack!')
     print("-------------------------")
-    bet = int(input("Please place your bet (min. $25) before playing: "))
-
-    if bet > user.balance:
-        print("Insufficient funds. Please add funds to your account.")
-        return
-
-    user.balance -= bet  # Reduce user's balance by the bet amount
-    print("Good Luck!")
-    deck = Deck()  # creates a deck
-    user = Player(deck,user.balance)  # creates a player that will use deck
-    pc = Dealer(deck,user.balance)  # creates a dealer that will use the same deck
+    deck = Deck()
+    user = Player(deck)
+    pc = Dealer(deck)
     user.see_cards()
     pc.see_card()
-    if user.win():  # checks user's hand to see if they win
+
+    if user.win():
         print("----------------------------------")
         pc.final_see_card()
         print("Blackjack! You win!")
-        winnings = int(bet * 2.5)
-        user.balance += winnings
-    elif pc.win():  # checks dealer's hand to see if they win
+    elif pc.win():
         print("----------------------------------")
         pc.final_see_card()
         print("The dealer won!")
     else:
-        while not user.lose():  # while user has not lost yet
-            choice = input("Hit (1), Stand (2), or Double Down (3)?: ")  # ask user to hit or stand
+        while not user.lose():
+            choice = input("Hit (1), Stand (2), or Double Down (3)?: ")
             print("-------------------------")
             user.see_cards()
             pc.see_card()
-            if choice == '1':  # hit
+            if choice == '1':
                 user.hit()
                 if user.value() > 21:
                     print("You bust! Dealer wins!")
                     user.see_cards()
                     pc.final_see_card()
                     break
-            elif choice == '2':  # stand
+            elif choice == '2':
                 user.stand()
                 pc.game(user)
                 break
-            elif choice == '3': # double down
-                if user.balance < bet:
-                    print('Please add funds to your account')
-                else:
-                    #line to double initial bet
-                    bet *= 2
-                    user.hit()
-                    user.see_cards()
-                    pc.game(user)
-            elif choice != '1' '2' '3':  # will ask user to try again if there is wrong input
+            elif choice == '3':
+                user.hit()
+                user.see_cards()
+                pc.game(user)
+            else:
                 print("Invalid input, please input 1, 2, or 3")
                 print("----------------------------------")
-                user.see_cards()  # shows user's cards
-                pc.see_card()  # shows dealer's card
-# Update user balance in the database
-cursor.execute("UPDATE user SET balance = ? WHERE username = ?", (user.balance, user.username))
-connector.commit()
+                user.see_cards()
+                pc.see_card()
+
 
 if __name__ == "__main__":
-    username = input("Enter your username: ")
-    cursor.execute("SELECT balance FROM users WHERE username = ?", (username,))
-    balance = cursor.fetchone()[0]
-    user = Player(Deck(), balance)
-    main(user)
+    main()
