4fe9762b0a5917afd3a7d0dc282187f083a92a46
secakusumae <secakusumae@wit.edu>
2023-08-30 12:15:01 -0400

    Added function to not allow user to register without username or password

--- login_system.py
+++ login_system.py
@@ -9,10 +9,8 @@ def existing_account():
     main_screen = Tk()
     main_screen.geometry("300x350")
     main_screen.title("Account Login")
-
     Label(text="Choose Login Or Register", bg="DeepSkyBlue2", width="300", height="2", font=("Calibri", 13)).pack()
     Label(text="").pack()
-
     login_button = PhotoImage(file="C:/Users/secakusumae/OneDrive - Wentworth Institute of Technology/Pictures/small_login.png")
     Button(main_screen, image=login_button, command=login).pack()
     Label(text="").pack()
@@ -41,7 +39,7 @@ def login():
     attempts = 0
     count = 1
 
-    Label(login_screen, text="Username * ").pack()
+    Label(login_screen, text="Username").pack()
     username_login_entry = Entry(login_screen, textvariable=username_verify)
     username_login_entry.pack()
     Label(login_screen, text="").pack()
@@ -51,7 +49,6 @@ def login():
     Label(login_screen, text="").pack()
     login_screen.bind('<Return>', lambda event: login_verification())
     Button(login_screen, text="Login", width=10, height=1, command=login_verification).pack()
-
 
 
 def register():
@@ -72,7 +69,7 @@ def register():
     password_register = StringVar()
     start_balance = IntVar()
 
-    Label(register_screen, text="Username * ").pack()
+    Label(register_screen, text="Username").pack()
     username_register_entry = Entry(register_screen, textvariable=username_register)
     username_register_entry.pack()
     Label(register_screen, text="").pack()
@@ -95,22 +92,20 @@ def registration_verification():
     cursor = conn.cursor()
     print('Successfully connected to database!')
 
-    if len(username_register_entry.get()) == 0:
-        ms.showerror("ERROR", "Registration failed!")
+    if len(username_register_entry.get()) == 0 or len(password_register.get()) == 0:
+        ms.showerror("ERROR", "Registration failed! Empty Username or Password")
         return None
+
     cursor.execute("SELECT username FROM user WHERE username = '{}'".format(username_register.get()))
     exists = cursor.fetchall()
     if exists:
         ms.showerror("ERROR", "USERNAME already exist")
         return None
 
-
     cursor.execute("INSERT into user VALUES('{}','{}','{}','{}')".format(username_register.get(), password_register.get(), start_balance.get(), 0))
     conn.commit()
     cursor.execute("SELECT balance FROM user WHERE username = '{}'".format(username_register.get()))
     new = cursor.fetchone()
-    # ADD VIP CHECK????
-
 
     if new:
         ms.showinfo("Congrats!", "Registration Successful!")
@@ -129,8 +124,7 @@ def login_verification():
     global count
     global attempts
 
-
-    if (count == 1):
+    if count == 1:
         attempts = 1
         count += 1
 
